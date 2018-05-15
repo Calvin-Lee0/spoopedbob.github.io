edddf4cf99e7bfb1cf6d24668e22b9d48d0e3bb1
Calvin Lee <calvin.lee@warriorlife.net>
2018-05-15 13:55:39 -0700

    Revert "j"
    This reverts commit be584e2fccdf0c56acd55e06487c1ca182a515be.

--- website/scripts/greeting.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-
-function greeting()  {
-		alert("wughefiuehgrviuehruigvhuerhvurhegirugfherufheiurhviergvugerhgouvhsiuhgiuhruitghw849u5ghw94h9awHetug	87TGWR8T87^t873rt8g	8374TQ8 8	T O972qgrf8ktgrq39b4tgqug34figq93uhbGW4FY4-[0gt 8	GT2890"); 
-}
